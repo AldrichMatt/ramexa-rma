0fb496c76dac73e6802a607619ab2bcd7e742147
Aldrich Matthew <mailbox.aldrich@gmail.com>
2023-09-11 17:22:01 +0800

    Item Done

--- controller/Item.php
+++ controller/Item.php
@@ -13,6 +13,23 @@ require "../autoload.php";
             $model->insertTableColVal('item', '`item_sn`, `item_category_id`', '"'.$sn.'", "'.$item_category.'"');
             break;
         
+        case 'delete':
+            $sn = $_POST['id'];
+            $model->deleteTableColVal('item', 'id', '"'.$sn.'"');   
+            break;
+            
+        case 'update':
+            $id = $_POST['id'];
+            $sn = $_POST['sn'];
+            $item_category = $_POST['item_category_id'];
+            try {
+                $model->updateTableSetWhere('item', '`item_sn` = "'.$sn.'", `item_category_id` = "'.$item_category.'"', '`id` = "'.$id.'"');
+                echo json_encode(["status"=>"succeed"]);
+            } catch (\Throwable $th) {
+                echo json_encode(["status"=>"failed"]);
+            }
+                break;
+            
         case 'getitem':
             $brand = $_POST['brand'];
             $item_data = $model->getAllWhere('item_category','`brand_id` = '.$brand.'');
--- item/add-item.php
+++ item/add-item.php
@@ -15,11 +15,38 @@ function getCorresponding($categoryId){
 ?>
 <script>
 
+  function updateItem(){
+    $('#modal_edit').addClass('hidden');
+    var id = $('input[name="id_update"]').val();
+    var item_category_id = $('select[name="item_name_update"]').val();
+    var sn = $('input[name="sn_update"]').val();
+    place = document.getElementById('item_place');
+    $.ajax({
+      method : 'POST',
+      url : '/ramexa-rma/ramexa-rma/controller/Item.php',
+      dataType : 'json',
+      data : {
+        action : 'update',
+        id : id,
+        item_category_id :item_category_id,
+        sn : sn,
+      },
+      complete : function(data){
+        if(data.responseJSON.status == "failed"){
+          alert("S/N already exists!");
+        }else{
+          alert("Item updated successfully");
+          $('#item_place').load(location + ' #item_place');
+        };
+      }
+    })
+                
+  }
 
-  function getItembyBrand(){
-    const item = document.getElementById('item_category');
-    item.innerHTML = '';
-    var brand = $('#brand').val();
+  function getItembyBrand(id, target){
+    const item = document.getElementById(target);
+    item.innerHTML = '<option value="">-</option>';
+    var brand = $('#'+id).val();
     $.ajax({
       method : 'POST',
       url : '/ramexa-rma/ramexa-rma/controller/Item.php',
@@ -35,6 +62,35 @@ function getCorresponding($categoryId){
       }
     });
 }
+
+function deleteItem(itemId){
+  $.ajax({
+    method :  'POST',
+    url : '/ramexa-rma/ramexa-rma/controller/Item.php',
+    dataType : 'json',
+    data : {
+      action : 'delete',
+      id :itemId,
+    },
+    complete : function(data){
+      $('#item_place').load(location + ' #item_place');
+    }
+  })
+  console.log(itemId);
+}
+function showModal(modal_name, data){
+                $('#modal_edit').removeClass('hidden');
+                $('input[name="id_update"]').val(data.id);
+                $('select[name="brand_name_update"] option:first').val(data.brand_id);
+                $('select[name="brand_name_update"] option:first').text(data.brand_name);
+                getItembyBrand('brand_update','item_update')
+                $('select[name="item_name_update"] option:first').val(data.category_id);
+                $('select[name="item_name_update"] option:first').text(data.category_name);
+                $('input[name="sn_update"]').val(data.sn);
+                // id_update,brand_name_update,item_name_update, sn_update
+                // id, sn,category_id,category_name,brand_id,brand_name
+                // item_warranty
+        }
 
   function inputsn(){
     var sn = $('#sn').val();
@@ -60,7 +116,7 @@ function getCorresponding($categoryId){
           sn : sn,
           item_category : item_category,
         },
-        complete : function(data){  470347
+        complete : function(data){ 
           $('#item_place').load(location + ' #item_place');
         }
       })
@@ -70,16 +126,52 @@ function getCorresponding($categoryId){
 </script>
     <div id="content" class="flex min-h-screen flex-col items-center justify-between md:px-4 sm:px-2">
       <div class="z-10 w-full max-w-5xl flex flex-col justify-center pt-16 text-sm ">
+      <div id="modal_edit" class="hidden">
+        <div class="flex justify-center items-center left-0 top-0 w-full h-full fixed z-20 bg-slate-500/70">
+          <div class="flex w-3/4 fixed justify-center text-center">
+            <div class="bg-slate-200 w-3/4 rounded-lg">
+              <div class="text-lg rounded-ss-lg rounded-se-lg text-white font-bold py-4 mb-6 bg-[#d93337]">
+                      Edit Item
+              </div>
+              <div class="px-4 my-8 text-start">
+                <input type="hidden" name="id_update">
+                <?=$component->label('Brand');?>
+                <div class="my-2">
+                        
+                  <select name="brand_name_update" onchange="getItembyBrand('brand_update','item_update')" id="brand_update" class="flex w-full min-h-[auto] border-0 justify-center rounded-md px-4 py-2 pl-5  focus:outline-[#d93337]">
+                    <option value="">-</option>
+                    <?php
+                    foreach($brandData as $brand):
+                            ?>
+                    <option value="<?=$brand[0]?>"><?=$brand[1]?></option>
+                    
+                    <?php endforeach;?>
+                  </select>
+                </div>
+                <?=$component->label('Item');?>
+                <div class="my-2">
+                  
+                  <select name="item_name_update" id="item_update" class="flex w-full min-h-[auto] border-0 justify-center rounded-md px-4 py-2 pl-5  focus:outline-[#d93337]">
+                    <option value="">-</option>
+                  </select>
+                </div>
+                <?=$component->input('S/N', 'xxx...', 'text', 'sn_update')?>
+                <button class="bg-[#d93337] h-10 px-4 my-2 rounded text-white" onclick="updateItem()">Update</button>
+                <button class="bg-slate-500 h-10 px-4 my-2 rounded text-white" onclick="$('#modal_edit').addClass('hidden')">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
         <?php
         $component->back("/ramexa-rma/ramexa-rma/item")
         ?>
         <p class="text-2xl font-semibold"><?=$title;?></p>
-        <div class="columns-2 gap-8">
             <?php
               $component->label("Brand");
             ?>
             <div class="my-2">
-              <select name="brand" onchange="getItembyBrand()" id="brand" class="flex w-full min-h-[auto] border-0 justify-center rounded-md px-4 py-2 pl-5 drop-shadow-[0_10px_10px_rgba(0,0,0,0.45)]  focus:outline-[#d93337]">
+              <select name="brand" onchange="getItembyBrand('brand','item_category')" id="brand" class="flex w-full min-h-[auto] border-0 justify-center rounded-md px-4 py-2 pl-5 drop-shadow-[0_10px_10px_rgba(0,0,0,0.45)]  focus:outline-[#d93337]">
                 <option value="">-</option>
                 <?php
                   foreach($brandData as $brand):
@@ -88,7 +180,7 @@ function getCorresponding($categoryId){
 
                 <?php endforeach;?>
               </select>
-            </div>
+            </div>   
             <?php
               $component->label("Item");
             ?>
@@ -103,8 +195,8 @@ function getCorresponding($categoryId){
             <div class="my-2">
               <input type="" name="sn" autofocus onchange="inputsn()" id="sn" class="flex w-full min-h-[auto] border-0 justify-center rounded-md px-4 py-2 pl-5 drop-shadow-[0_10px_10px_rgba(0,0,0,0.45)]  focus:outline-[#d93337]">
             </div>
-            </div>
-            <div class="my-8" id="item_place">
+            <p class="text-neutral-400">Press ENTER to input manually</p>
+            <div class="mt-2" id="item_place">
             <div id="table">
                   <table class="table-auto mt-2 bg-white rounded-ss-lg w-full h-full rounded-se-lg drop-shadow-[0_35px_35px_rgba(0,0,0,0.45)] ">
                           <thead class="py-2 my-2 h-10">
@@ -113,7 +205,6 @@ function getCorresponding($categoryId){
                                 <th>S/N</th>
                                 <th>Item Category</th>
                                 <th>Brand</th>
-                                <th>Status</th>
                                 <th>Warranty</th>
                                 <th>Action</th>
                         </tr>
@@ -129,12 +220,17 @@ function getCorresponding($categoryId){
                                         <td id="item_sn"><?=$item[1];?></td>
                                         <td id="item_category"><?=$category[1];?></td>
                                         <td id="item_brand"><?=$model->getWhere('brand','brand_name','`id` = '.$category[2].'')[0];?></td>
-                                        <td id="item_status"><?=$category[4];?></td>
-                                        <td id="item_warranty"><?=$category[5];?></td>
+                                        <td id="item_warranty"><?php 
+                                        if($category[3] == 0){
+                                                echo '-';
+                                        } else{       
+                                        echo $model->getAllWhere("warranty","`id` = '".$category[3]."'")[0][1];
+                                        }
+                                        ?></td>
                                         <td>
                                         <button
                                         class="w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 sm:w-1/3 h-8 justify-center inline-block rounded bg-[#d93337] focus:outline-[#d93337]"
-                                        onclick="deleteBrand('<?=$brand[0];?>','<?=$brand[1];?>')">
+                                        onclick="deleteItem('<?=$item[0];?>')">
                                                 <img
                                                 class="px-auto mx-auto"
                                                 src = "/ramexa-rma/ramexa-rma/assets/trash.png"
@@ -145,7 +241,7 @@ function getCorresponding($categoryId){
                                         </button>
                                         <button
                                         class="w-1/3 md:w-1/4 lg:w-1/5 xl:w-1/6 sm:w-1/3 h-8 justify-center inline-block rounded bg-[#ffb60d] focus:outline-[#d93337]"
-                                        onclick="showModal('modal_edit', {id : '<?=$brand[0]?>', brand_name : '<?=$brand[1]?>'})">
+                                        onclick="showModal('modal_edit', {id : '<?=$item[0]?>', sn : '<?=$item[1]?>', category_id : '<?=$item[2]?>',category_name : '<?=$category[1]?>', warranty_id : '<?=$category[3]?>', brand_id : '<?=$model->getWhere('brand','*','`id` = '.$category[2].'')[0];?>', brand_name : '<?=$model->getWhere('brand','*','`id` = '.$category[2].'')[1];?>'})">
                                                 <img
                                                 class="px-auto mx-auto"
                                                 src = "/ramexa-rma/ramexa-rma/assets/pencil.png"
